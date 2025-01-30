c53fd2794569b7efbc7e39467763e945f1d75b61
fajar ramadhan <96865504+Fajarr021123@users.noreply.github.com>
2025-01-30 11:24:50 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function () {
-    Route::get('/', [AdminController::class, 'index']); // Get all admins
-    Route::post('/', [AdminController::class, 'store']); // Create admin
-    Route::get('/{id}', [AdminController::class, 'show']); // Get single admin
-    Route::put('/{id}', [AdminController::class, 'update']); // Update admin
-    Route::delete('/{id}', [AdminController::class, 'destroy']); // Delete admin
+    Route::get('/', [AdminController::class, 'index']); 
+    Route::post('/', [AdminController::class, 'store']); 
+    Route::get('/{id}', [AdminController::class, 'show']); 
+    Route::put('/{id}', [AdminController::class, 'update']);
+    Route::delete('/{id}', [AdminController::class, 'destroy']); 
 });
